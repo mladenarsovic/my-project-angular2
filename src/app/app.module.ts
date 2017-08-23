a76cffe98ca08c5283dccfeee47517413f954102
Mladen <djoml8bre@gmail.com>
2017-08-23 21:15:46 +0200

    Dodate rute

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 
-import { ContactsComponent } from './contacts/contacts.component';
-import { LayoutComponent } from './layout/layout.component'; 
+import { ContactsComponent } from './components/contacts/contacts.component';
+import { LayoutComponent } from './components/layout/layout.component';
+import { MessagesComponent } from './components/messages/messages.component'; 
+
+const appRoutes: Routes = [
+	{
+		path: '',
+		redirectTo: '/contacts',
+		pathMatch: 'full'
+	},
+	{
+		path: 'contacts',
+		component: ContactsComponent
+	},
+	{
+		path: 'messages',
+		component: MessagesComponent
+	}
+];
 
 @NgModule({
   declarations: [
-    AppComponent, ContactsComponent, LayoutComponent				
+    AppComponent, ContactsComponent, LayoutComponent, MessagesComponent				
   ],
   imports: [
+  	RouterModule.forRoot(
+  		appRoutes
+  		),
     BrowserModule
   ],
   providers: [],
